5f8231e5b3c8ada8204cda4cc1aa655385ddc7c6
Guilherme Ramos Mendes <guilhermerm.ciandt@sulamerica.com.br>
2020-12-05 21:30:01 -0300

    Solution

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace hacker_rank_2d_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 1 1 0 0 0
+            // 0 1 0 0 0 0
+            // 1 1 1 0 0 0
+            // 0 0 2 4 4 0
+            // 0 0 0 2 0 0
+            // 0 0 1 2 4 0
+
+            int[][] arr = new int[6][];
+            arr[0] = new int[] { 1, 1, 1, 0, 0, 0 };
+            arr[1] = new int[] { 0, 1, 0, 0, 0, 0 };
+            arr[2] = new int[] { 1, 1, 1, 0, 0, 0 };
+            arr[3] = new int[] { 0, 0, 2, 4, 4, 0 };
+            arr[4] = new int[] { 0, 0, 0, 2, 0, 0 };
+            arr[5] = new int[] { 0, 0, 1, 2, 4, 0 };
+
+            int sum = hourglassSum(arr);
+            Console.WriteLine($"Max hourglass sum {sum}");
+        }
+
+        static int hourglassSum(int[][] arr)
+        {
+            int rows = arr.Length;
+            int columns = arr[0].Length;
+            int max_hourglass_sum = -63;
+
+            for (int i = 0; i < rows - 2; i++)
+            {
+                for (int j = 0; j < columns - 2; j++)
+                {
+                    int current_max_sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] +
+                    arr[i + 2][j + 2];
+                    max_hourglass_sum = Math.Max(max_hourglass_sum, current_max_sum);
+                }
+            }
+            return max_hourglass_sum;
+        }
+    }
+}
