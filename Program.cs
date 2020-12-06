07fb1e2f1b11b4cdfd25d6218e1515e2bb6ff23a
Guilherme Ramos Mendes <guilhermerm.ciandt@sulamerica.com.br>
2020-12-05 21:41:41 -0300

    Refactoring

--- Program.cs
+++ Program.cs
@@ -21,22 +21,25 @@ namespace hacker_rank_2d_array
             arr[4] = new int[] { 0, 0, 0, 2, 0, 0 };
             arr[5] = new int[] { 0, 0, 1, 2, 4, 0 };
 
-            int sum = hourglassSum(arr);
-            Console.WriteLine($"Max hourglass sum {sum}");
+            int sum = HourglassMaxSum(arr);
+            Console.WriteLine($"Max hourglass sum is {sum}");
         }
 
-        static int hourglassSum(int[][] arr)
+        private static int HourglassMaxSum(int[][] arr)
         {
             int rows = arr.Length;
             int columns = arr[0].Length;
+            //constraint 7 * -9 = -63 (Minimum value possible)
             int max_hourglass_sum = -63;
 
             for (int i = 0; i < rows - 2; i++)
             {
                 for (int j = 0; j < columns - 2; j++)
                 {
-                    int current_max_sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] +
-                    arr[i + 2][j + 2];
+                    int top = (arr[i][j] + arr[i][j + 1] + arr[i][j + 2]);
+                    int middle = arr[i + 1][j + 1];
+                    int bottom = (arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]);
+                    int current_max_sum = (top + middle + bottom);
                     max_hourglass_sum = Math.Max(max_hourglass_sum, current_max_sum);
                 }
             }
